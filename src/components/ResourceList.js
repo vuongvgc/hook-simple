d8bcb25f3370a580c3930f3a9ea54f4511f200be
vuongvgc <vuongvgc@gmail.com>
2020-12-23 22:21:24 +0700

    Fetching date from API: immediate invoke function js in useEffect

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -3,14 +3,20 @@ import axios from "axios";
 const ResourceList = ({ resource }) => {
   console.log(resource);
   const [resources, setResource] = useState([]);
-  const fetchResource = async (resource) => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-    setResource(response.data);
-  };
+  //   const fetchResource = async (resource) => {
+  //     const response = await axios.get(
+  //       `https://jsonplaceholder.typicode.com/${resource}`
+  //     );
+  //     setResource(response.data);
+  //   };
   useEffect(() => {
-    fetchResource(resource);
+    (async () => {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${resource}`
+      );
+      setResource(response.data);
+    })(resource);
+    // immediate invoke function js
   }, [resource]);
 
   return <div>{resources.length}</div>;
